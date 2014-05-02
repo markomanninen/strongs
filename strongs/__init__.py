36bbf9268f87c72a4e4745a70787ed4f176337a0
Marko Manninen <mmstud@gmail.com>
2014-05-02 20:11:55 +0300

    Major structural change
    HTML generation and JS code moved from notebooks to module for cleaner
    end user experience

--- strongs/__init__.py
+++ strongs/__init__.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 # file: __init__.py
 
-from main import load_dataframe, find, preprocess_greek, search_strongs_dictionary_html
+from main import load_dataframe, find, preprocess_greek, \
+                 search_strongs_dictionary_html, \
+                 print_search_form_html, print_search_form_js
 
 load_dataframe(csv = "strongs/nt-strongs.csv")
--- strongs/main.py
+++ strongs/main.py
@@ -7,6 +7,7 @@ import pandas as pd
 from os.path import isfile
 from tagpy import helper as h, table
 from isopsephy import preprocess_greek, to_roman, isopsephy
+from IPython.display import HTML
 
 dictionary = pd.DataFrame()
 
@@ -44,3 +45,59 @@ def search_strongs_dictionary_html(query, field):
     # using print data stream instead of returning data makes 
     # less hassle with unicodes and character encodings
     print str(search_strongs_dictionary_table(query, field))
+
+#
+def print_search_form_html():
+    form = """
+<h3>Search</h3>
+
+<div class="search-input"><span>Greek</span> <input type="text" id="word" value="Αρμαγεδδων" /> <button onclick="search_strongs_dictionary('word')">Retrieve</button></div>
+<div class="search-input"><span>Roman</span> <input type="text" id="transliteration" value="aarwn" /> <button onclick="search_strongs_dictionary('transliteration')">Retrieve</button></div>
+<div class="search-input"><span>Lemma</span> <input type="text" id="lemma" value="G2424" /> <button onclick="search_strongs_dictionary('lemma')">Retrieve</button></div>
+<div class="search-input"><span>Isopsephy</span> <input type="text" id="isopsephy" value="777" /> <button onclick="search_strongs_dictionary('isopsephy')">Retrieve</button></div>
+<div class="search-input"><span>Translation</span> <input type="text" id="translation" value="flute" /> <button onclick="search_strongs_dictionary('translation')">Retrieve</button></div>
+
+<h3>Results</h3>
+
+<div id="result">
+
+<table class="data"><thead><tr><th>Lemma</th><th>Word</th><th>Transliteration</th><th>Translation</th><th>Isopsephy</th></tr></thead><tbody><tr><td>G717</td><td>Αρμαγεδδων</td><td>Armageddwn</td><td>Armageddon (or Har-Meggiddon), a symbolic name</td><td>1008</td></tr></tbody></table>
+
+</div>
+
+<style>
+div.search-input span {height: 16px; width: 80px; display: block; float: left;}
+div.search-input input {height: 16px;}
+div.search-input button {margin-top: -9px;}
+</style>
+"""
+    return HTML(form)
+
+def print_search_form_js():
+    js = """
+<script>
+
+function handle_output(out_type, out) {
+    var res = null
+    if (out_type == "stream") {
+        res = out.data
+    } else if (out_type == "pyout") {
+        res = out.data["text/plain"]
+    } else if (out_type == "pyerr") {
+        res = out.ename + ": " + out.evalue
+    } else {
+        res = "[output type: "+out_type+" is not implemented]";
+    }
+    document.getElementById("result").innerHTML = res
+}
+
+var kernel = IPython.notebook.kernel
+var callbacks = {'output' : handle_output}
+
+function search_strongs_dictionary(field) {
+    kernel.execute("search_strongs_dictionary_html('"+document.getElementById(field).value+"', '"+field+"')", callbacks, {silent:false})
+}
+
+</script>
+"""
+    return HTML(js)
